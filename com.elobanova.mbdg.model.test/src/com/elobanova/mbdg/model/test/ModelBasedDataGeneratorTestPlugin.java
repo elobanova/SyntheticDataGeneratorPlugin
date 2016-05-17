d28f562878f51849c7080b8b072e8fc89b431caa
Ekaterina Lobanova <ekaterina.lobanova@rwth-aachen.de>
2016-05-17 16:36:39 +0200

    Created an Eclipse Plugin for Synthetic Data Generation Model and a Test PLugin for it.

---
+++ com.elobanova.mbdg.model.test/src/com/elobanova/mbdg/model/test/ModelBasedDataGeneratorTestPlugin.java
@@ -0,0 +1,30 @@
+package com.elobanova.mbdg.model.test;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class ModelBasedDataGeneratorTestPlugin implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		ModelBasedDataGeneratorTestPlugin.context = bundleContext;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+		ModelBasedDataGeneratorTestPlugin.context = null;
+	}
+
+}
